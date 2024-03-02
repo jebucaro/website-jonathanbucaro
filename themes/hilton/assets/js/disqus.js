3fec5590aed0613b78e32bd26f479c83e4e8a1e5
Jonathan Búcaro <hola@jonathanbucaro.com>
2024-03-01 21:59:02 -0600

    :bento: moved scripts to individual files

---
+++ themes/hilton/assets/js/disqus.js
@@ -0,0 +1,19 @@
+  var disqus_loaded = false;
+  var disqus_shortname = '{{ .Site.Params.disqus.disqusShortname }}';
+  var disqus_container = document.getElementById("disqus_thread");
+  function disqus() {
+    if (!disqus_loaded) {
+      disqus_loaded = true;
+      var e = document.createElement("script");
+      e.type = "text/javascript";
+      e.async = true;
+      e.src = "//" + disqus_shortname + ".disqus.com/embed.js";
+      (document.getElementsByTagName("head")[0] ||
+        document.getElementsByTagName("body")[0])
+        .appendChild(e);
+      // Hide the button after opening
+      document.getElementById("show-comments-button").style.display = "none";
+      // Show disqus comments
+      disqus_container.classList.add("is-open");
+    }
+  }
