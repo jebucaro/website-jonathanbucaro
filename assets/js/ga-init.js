86c9bd467b3a935ca6fca65d62a95c39eb3ce99f
Jonathan Búcaro <jebucaro@users.noreply.github.com>
2026-05-28 09:23:18 -0600

    :bug: fix google analytics blocked by stale consent mode defaults (#146)

--- assets/js/ga-init.js
+++ assets/js/ga-init.js
@@ -1,20 +1,10 @@
-// Initialize Google Analytics 4 with privacy settings
 window.dataLayer = window.dataLayer || [];
 function gtag() {
     dataLayer.push(arguments);
 }
 
-// Set Consent Mode v2 defaults (deny all until user consents)
-gtag('consent', 'default', {
-    analytics_storage: 'denied',
-    ad_storage: 'denied',
-    ad_user_data: 'denied',
-    ad_personalization: 'denied',
-});
-
 gtag('js', new Date());
 
-// Configure GA4 with privacy-respecting settings
 gtag('config', '{{ .Site.Config.Services.GoogleAnalytics.ID }}', {
     anonymize_ip: true,
 });
